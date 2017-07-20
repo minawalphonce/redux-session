7c1ab3e35425361c393e0ac47c6e8a28d480caba
Mina W. Alphonce <mina.alphonce@malphonce.com>
2017-07-20 12:05:10 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rs-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'rs works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { ListComponent } from './containers/list/list.component';
+import { DetailsComponent } from './containers/details/details.component';
+
+export const router: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
+    RouterModule.forRoot(router),
     BrowserModule,
     FormsModule,
     HttpModule
